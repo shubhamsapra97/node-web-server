8f0feabb8628879957ae8e7daae5be10f9560bea
shubhamsapra97 <shubham.sapra97@gmail.com>
2017-07-01 00:04:04 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine' , 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear' , ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt' , (text)=> {
+  return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log' , log + '\n' , (err) => {
+    if(err){
+      console.log('Unable to write to file');
+    }
+    next();
+  });
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintainance.hbs');
+// });
+
+app.get('/' , (req,res) => {
+  // res.send({
+  //   name: 'Shubham',
+  //   likings: [
+  //     'biking',
+  //     'eating'
+  //   ]
+  // });
+  res.render('home.hbs' , {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to Home page'
+  })
+});
+
+app.get('/about' , (req , res) => {
+  res.render('about.hbs' , {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad' , (req , res) => {
+  res.send({
+    errorMessage: 'Unable to process the message'
+  });
+});
+
+app.listen(3000 , () => {
+  console.log('Server is up on Port 3000');
+});
